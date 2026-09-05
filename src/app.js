e12dc29b141e8bff647a358f7aae973ae33eba4c
YuvrajVashist <yuvrajvashist001@gmail.com>
2026-09-05 16:48:57 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ import cookieParser from "cookie-parser"
 const app = express()
 //allowing which frontend can access our backend
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 })
 )
 //this step is use to set the limit of data sent to server
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true }))
 //jsut to store the images video pdf 
 app.use(express.static("public"))
 app.use(cookieParser())
@@ -19,7 +19,7 @@ app.use(cookieParser())
 //routes
 import userRouter from "./routes/user.routes.js"
 
-app.use("/api/v1/user",userRouter)
+app.use("/api/v1/user", userRouter)
 
 
 export default app
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@
 
 //   (fn)=>{()=>{}}
 const asyncHandler = (requestHandler)=>{
-    return (req,res,next) =>{
+    return(req,res,next) =>{
         Promise.resolve(requestHandler(req,res,next))
         .catch((err)=>next(err))
     }
